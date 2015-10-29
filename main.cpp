3dcd0a80904be59cbee4dfc1b47d7cf4b4565fb1
RiMillo <ricc.milani@gmail.com>
2015-10-29 17:06:48 +0100

    Branch and first commit
    Like in subjet

--- main.cpp
+++ main.cpp
@@ -8,7 +8,9 @@ int main()
 {
 
 
-    cout <<  “Ciao Millo! << endl;
+    cout <<  "Ciao Millo!" << endl;
+    cout << "I wanna have a branch for only myself!" <<endl;
+    cout << "And now a commit on my branch" <<endl;
 
     return 0;
 }
